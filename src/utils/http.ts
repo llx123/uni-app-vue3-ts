6d80da06033706f88cbb9cceb7ee606330d23526
lxli <573381010@qq.com>
2023-12-06 01:44:19 +0800

    feat: 拦截器

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,12 +1,27 @@
+import { useMemberStore } from "@/stores"
+
 const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
 
 // 添加拦截器
 const httpInterceptor = {
   invoke(options: UniApp.RequestOptions) {
+    // 非 http 开头需要拼接
     if (!options.url.startsWith('http')) {
       options.url = baseURL + options.url
     }
+    // 请求超时
     options.timeout = 10000
+    // 小程序标识头
+    options.header = {
+      ...options.header,
+      'source-client': 'miniapp',
+    }
+    // token 标识头
+    const meberStore = useMemberStore()
+    const token = meberStore?.profile?.token
+    if (token) {
+      options.header.Authorization = token
+    }
   },
 }
 uni.addInterceptor('request', httpInterceptor)
