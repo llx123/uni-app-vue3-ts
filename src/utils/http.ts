00e6bd629039260900da938804b240860f9b3ced
lxli <573381010@qq.com>
2023-12-06 01:34:37 +0800

    feat: http拦截器

---
+++ src/utils/http.ts
@@ -0,0 +1,13 @@
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+// 添加拦截器
+const httpInterceptor = {
+  invoke(options: UniApp.RequestOptions) {
+    if (!options.url.startsWith('http')) {
+      options.url = baseURL + options.url
+    }
+    options.timeout = 10000
+  },
+}
+uni.addInterceptor('request', httpInterceptor)
+uni.addInterceptor('uploadFile', httpInterceptor)
