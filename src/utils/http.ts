b98229b680999a615d4804f2db91d41db247eada
lxli <573381010@qq.com>
2023-12-06 02:09:10 +0800

    feat: 请求成功处理

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,7 +2,7 @@ import { useMemberStore } from "@/stores"
 
 const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
 
-// 添加拦截器
+// 添加请求拦截器
 const httpInterceptor = {
   invoke(options: UniApp.RequestOptions) {
     // 非 http 开头需要拼接
@@ -26,3 +26,24 @@ const httpInterceptor = {
 }
 uni.addInterceptor('request', httpInterceptor)
 uni.addInterceptor('uploadFile', httpInterceptor)
+
+/**
+ * 请求函数
+ * @param UniApp.RequestOptions
+ * @returns Promise
+ */
+interface Data<T> {
+  code: string
+  msg: string
+  result: T
+}
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success(res) {
+        resolve(res.data as Data<T>)
+      },
+    })
+  })
+}
